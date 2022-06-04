ab5b23e7306dddf509a3399d63dd6abac5132560
Jose Luis Mirafuentes <joseluis0528@gmail.com>
2022-06-04 13:33:50 -0500

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,26 +1,44 @@
 import React from "react";
 
-function ColorList() {
+function ColorList(props) {
   const colors = [
     "firebrick",
     "rebeccapurple",
     "salmon",
     "darkslategray",
     "hotpink",
+    "navyblue",
+    "olivegreen"
   ];
 
+  // return (
+  //   <div>
+  //     <h1>Top 5 CSS Colors</h1>
+  //     <ol>
+  //       <li style={{ color: colors[0] }}>{colors[0]}</li>
+  //       <li style={{ color: colors[1] }}>{colors[1]}</li>
+  //       <li style={{ color: colors[2] }}>{colors[2]}</li>
+  //       <li style={{ color: colors[3] }}>{colors[3]}</li>
+  //       <li style={{ color: colors[4] }}>{colors[4]}</li>
+  //     </ol>
+  //   </div>
+  // );
+
+  const colorElements = colors.map((color) => {
+    return <li key={color} /*add key prop to fix error msg*/ style={{ color: color }}>{color}</li>;
+  });
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {colorElements}
       </ol>
     </div>
-  );
+  )
+
+  // const colorElements = colors.map((color) => {
+  //   return <ColorItem key={color} color={color} />;
+  // });
 }
 
 export default ColorList;
